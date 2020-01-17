8c713230b229a1e33283f409a3cda7a8caf647d7
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-01-17 21:13:50 +0200

    new problem

---
+++ Solutions/Sum of Two Integers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+const getSum = function(a, b) {
+    while (b !=  0)  
+    {   
+        let carry = a & b;  
+        a = a ^ b;   
+        b = carry << 1;  
+    }  
+    return a;
+};
