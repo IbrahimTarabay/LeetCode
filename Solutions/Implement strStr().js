109cffaac451d114b67e8aa6caacdf4d929ecd8d
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-02-21 08:33:54 +0200

    new problem

---
+++ Solutions/Implement strStr().js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (!needle.length) return 0;
+    for (let i in haystack) if (haystack[i] === needle[0] && haystack.substring(i, Number(i) + needle.length) === needle) return i;
+    return -1;
+};
