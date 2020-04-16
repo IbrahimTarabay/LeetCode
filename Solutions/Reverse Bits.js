7a5d0c8552caf3820293f995e0fd7fe349576ad2
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-04-16 20:42:19 +0200

    new problem

---
+++ Solutions/Reverse Bits.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+const reverseBits = function(n) {
+    let ret = 0;
+    let i=0;
+  
+    while(n){
+        i++;
+        ret = (ret<<1) | (n&1);
+        n=(n>>>1);
+    }
+    
+    if(i<32){
+        ret = ret << (32-i);
+    }
+  
+    return ret >>> 0;
+  };
