c26632d6ae37f72f0580fed9329c8a7e1e876840
Ibrahim_Tarabay <32605959+IbrahimTarabay@users.noreply.github.com>
2020-06-02 23:56:43 +0200

    Create Quick-Sort.js

---
+++ Algorithms+Data-Structure/Quick-Sort.js
@@ -0,0 +1,47 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right){
+  const len = array.length; 
+  let pivot;
+  let partitionIndex;
+
+  if(left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    console.log(partitionIndex);
+    //sort left and right
+    if(partitionIndex-1>left){
+    console.log(quickSort(array, left, partitionIndex - 1));
+    }
+   
+   if(partitionIndex+1<right){
+    console.log(quickSort(array, partitionIndex + 1, right)+"hema");
+   }
+  }
+  //console.log(array);
+  return array;
+}
+   
+function partition(array, pivot, left, right){
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for(let i = left; i < right; i++) {
+    if(array[i] < pivotValue){
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+//console.log(numbers);
